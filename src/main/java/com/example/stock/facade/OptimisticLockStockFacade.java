ed2f8a64baf2e8e563c81c017f9d1bd3fb3c1576
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-11-19 21:27:31 +0900

    ✨ pessimistic lock
    - Entity변경(낙관적 락 버저닝)
    - facade패턴 구현 활용
        - facade 및 optimistic service 구현
    - Test

---
+++ src/main/java/com/example/stock/facade/OptimisticLockStockFacade.java
@@ -0,0 +1,28 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.OptimisticLockStockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OptimisticLockStockFacade {
+
+    private final OptimisticLockStockService optimisticLockStockService;
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        while(true) {
+            try{
+                optimisticLockStockService.decrease(id, quantity);
+
+                // 정상적으로 업데이트되면 나가기
+                break;
+            } catch(Exception e){
+                // 실패하면 50밀리초 기다렸다가 다시 시도한다.
+                Thread.sleep(50);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/stock/service/OptimisticLockStockService.java
@@ -0,0 +1,26 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OptimisticLockStockService {
+    private final StockRepository stockRepository;
+
+    @Transactional
+    public synchronized void decrease(Long id, Long quantity){
+        // get stock
+        Stock stock = stockRepository.findByIdWithOptimisticLock(id);
+
+        // decrease stock
+        stock.decrease(quantity);
+
+        // save
+        stockRepository.saveAndFlush(stock);
+    }
+}
